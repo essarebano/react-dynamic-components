93baad6ed73c91fa27774a5c2ee26431f7c01369
essarebano <essarebano.dev@gmail.com>
2024-05-21 01:25:32 +0800

    Added react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import ErrorPage from './error-page'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
